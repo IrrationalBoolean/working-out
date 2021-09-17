f17136a1a892c2c414bc17b414a115cfc01f4618
KJ <irateboolean@gmx.com>
2021-09-17 16:29:01 -0400

    merged db utility functions

--- db_utils.py
+++ db_utils.py
@@ -1,11 +1,8 @@
 import sqlite3
 
-from db_query import check_table
 
-
-def _create_table_pushup(con) -> None:
+def _create_table_pushup(cur) -> None:
     """creates table for pushups if table does not exist"""
-    cur = con.cursor()
     cur.execute('''
     CREATE TABLE IF NOT EXISTS pushup
         ( id INTEGER PRIMARY KEY
@@ -20,9 +17,14 @@ def _create_table_pushup(con) -> None:
     ''')
 
 
-def _create_table_plank(con) -> None:
+def insert_pushup_row(cur, data):
+    cur.execute("""
+    INSERT INTO pushup (date, time, dur, qty, uid)
+    VALUES(?,?,?,?,?)""", data)
+
+
+def _create_table_plank(cur) -> None:
     """creates table for planks if table does not exist"""
-    cur = con.cursor()
     cur.execute('''
     CREATE TABLE IF NOT EXISTS plank
         ( id INTEGER PRIMARY KEY
@@ -35,25 +37,45 @@ def _create_table_plank(con) -> None:
     ''')
 
 
-def _create_table_user(con) -> None:
+def _create_table_user(cur) -> None:
     """creates table for users if table does not exist"""
-    cur = con.cursor()
     cur.execute('''
     CREATE TABLE IF NOT EXISTS user
     (id INTEGER PRIMARY KEY, name TEXT)
     ''')
 
 
+
+def check_table(cur, table: str) -> bool:
+    """checks if table exists in database, returns boolean"""
+    table_data = cur.execute(f"""
+    SELECT name 
+    FROM sqlite_master 
+    WHERE type='table' 
+      AND name='{table}'
+    """)
+    table_fetch = table_data.fetchall()
+    if table_fetch:
+        return True
+    return False
+
+
+def select_all(cur, table) -> list:
+    """returns 'SELECT * FROM table'"""
+    cur.execute(f'''SELECT * FROM {table}''')
+    return cur.fetchall()
+
+
 def start_app() -> sqlite3.Connection:
     """creates tables if they don't exist, returns connection to database"""
-    con = sqlite3.connect('test.db')
+    con = sqlite3.connect('data.db')
     cur = con.cursor()
     cur.execute('PRAGMA foreign_keys = ON')
     con.commit()
-    if not check_table(con, 'user'):
-        _create_table_user(con)
-    if not check_table(con, 'pushup'):
-        _create_table_pushup(con)
-    if not check_table(con, 'plank'):
-        _create_table_plank(con)
+    if not check_table(cur, 'user'):
+        _create_table_user(cur)
+    if not check_table(cur, 'pushup'):
+        _create_table_pushup(cur)
+    if not check_table(cur, 'plank'):
+        _create_table_plank(cur)
     return con
